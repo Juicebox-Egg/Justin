ca0807f23b99fdd3bd4b0824b77eaa007a3e1586
Juicebox-Egg <justinhi9809@gmail.com>
2026-07-20 15:01:59 +1200

    ALL tetxure/design and Checkpoint Works

--- project/Assets/Scripts/main_menu.gd
+++ project/Assets/Scripts/main_menu.gd
@@ -1,5 +1,9 @@
 extends Node
 
+func _load_level() -> void:
+	GlobalScript.checkpoint_pos = Vector2(-999, -999)
+	GlobalScript.previous_checkpoint_node = null
+	get_tree().change_scene_to_file("res://Assets/Scenes/main_menu.tscn")
 
 func _on_world_0_pressed() -> void:
 	get_tree().change_scene_to_file("res://Assets/Scenes/Worlds/word0.tscn")
--- project/Assets/Scripts/player_controller.gd
+++ project/Assets/Scripts/player_controller.gd
@@ -4,32 +4,42 @@ class_name PlayerController
 @export var speed = 10.0
 @export var jump_power = 10.0
 
-var speed_multiplier = 30.0
+var speed_multiplier = 20.0
 var jump_multiplier = -30.0
 var direction = 0
 
+
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-#simple dash
-const DASH_SPEED = 50.0
+# simple dash
+const DASH_SPEED = 40.0
 var dashing = false
 var can_dash = true
 
-#spikes
+# spikes
 var took_damage = false
 var can_move = true
 
-#coyote jump
+# coyote jump
 var coyote_jump: bool = false
 var jumping: bool = false
 var coyote_frames: int = 5
 var was_on_floor: bool = false
 @onready var coyote_timer = $CoyoteTimer
 
-#coyote framerate
+#wall jump
+const wall_jump_pushback = 100
+
+#checkpoint
+@export var player_checkpont_pos: Vector2 = Vector2(-999, -999)
+
+# coyote framerate
 func _ready() -> void:
 	coyote_timer.wait_time = coyote_frames / 60.0
-
+	add_to_group("player")
+	
+	if GlobalScript.checkpoint_pos != Vector2(-999, -999):
+		global_position = GlobalScript.checkpoint_pos
 
 func _input(event):
 # Handle jump.
@@ -43,7 +53,7 @@ func _input(event):
 	else:
 		set_collision_mask_value(10, true)
 
-#dash
+# dash
 	if Input.is_action_just_pressed("dash") and can_dash:
 		dashing = true
 		can_dash = false
@@ -52,13 +62,13 @@ func _input(event):
 		$dash_again.start()
 		
 
-#respawn
-func respawn():
+# respawn
+func respawn(respawn_pos:Vector2):
 	self.visible = false
 	can_move = false
 	await get_tree().create_timer(0.5).timeout
 	
-	self.global_position = Vector2(300, 220)
+	self.global_position = Vector2(respawn_pos)
 	self.visible = true
 	can_move = true
 	await get_tree().create_timer(0.5).timeout
@@ -73,17 +83,18 @@ func _physics_process(delta: float) -> void:
 		#print(velocity.y) was elif below
 	elif not is_on_floor():
 		velocity += get_gravity() * delta
+	
 
-#Spikes
+# Spikes
 	for i in get_slide_collision_count():
 		var _collision = get_slide_collision(i)
 		
 		if _collision.get_collider().name == "Spikes": 
 			if took_damage == false:
 				took_damage = true
-				respawn()
+				respawn(player_checkpont_pos)
 
-#Basic Movement
+# Basic Movement
 	if can_move == false:
 		return
 	else:
@@ -98,7 +109,7 @@ func _physics_process(delta: float) -> void:
 	
 	move_and_slide()
 	
-#coyote jump
+# coyote jump
 	if is_on_floor() and jumping:
 		jumping = false
 	if was_on_floor and !is_on_floor() and not jumping:
@@ -106,13 +117,24 @@ func _physics_process(delta: float) -> void:
 		coyote_timer.start()
 	was_on_floor = is_on_floor()
 
+# wall_jump?
+func jump():
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor():
+			velocity.y = jump_power * jump_multiplier
+		if is_on_wall() and Input.is_action_just_pressed("move_right"):
+			velocity.y = jump_power * jump_multiplier
+			velocity.x = -wall_jump_pushback
+		if is_on_wall() and Input.is_action_just_pressed("move_left"):
+			velocity.y = jump_power * jump_multiplier
+			velocity.x = wall_jump_pushback
 
-#stops dashing
+# stops dashing
 func _on_dash_timer_timeout() -> void:
 	dashing = false
-#to dash again
+# to dash again
 func _on_dash_again_timeout() -> void:
 	can_dash = true
-#coyote timer
+# coyote timer
 func _on_coyote_timer_timeout() -> void:
 	coyote_jump = false
